47da85c3ae179acc95dead350202b4cd36d476a0
Alexander Schlagan <alexander.schlagan@gmail.com>
2026-06-15 18:55:12 +0200

    API URL new version updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
 
   const fetchCountries = async () => {
     try {
-      const response = await fetch("https://restcountries.com/v3.1/all?fields=name,flags,region,population,area")
+      const response = await fetch("https://restcountries.com/v3.1/all")
       const data = await response.json()
       setCountries(data)
       setIsLoading(false)
